749b54ef56dbcf6fdece7f8733a703b246146238
Mario-Milchevski <mario.milchevski@gmail.com>
2024-03-08 00:48:33 +0100

    Tiny bugs resolved within the filter

--- nodeJS_Basic/Homework_3_Express/services/filter.service.js
+++ nodeJS_Basic/Homework_3_Express/services/filter.service.js
@@ -1,34 +1,34 @@
+import logger from '../services/logger.service.js';
+
 export function filterTrainers(trainers, params) {
     let filteredTrainers = trainers;
 
     // Filter by currentlyActive status with query params
-    if (!params.currentlyActive && !params.sortBy) {
-        // logger.emit('log', `GET All students.`);
-        //i don't know why this couldn't work ------------------ when i run the debugger before it enters the 
-        // logger emit it goes to a file named "layer.js" from express and something is going on there
+    if (params.currentlyActive === undefined && params.sortBy === undefined) {
+        logger.emit('log', `GET All trainers.`);
         return filteredTrainers;
     }
     if (params.currentlyActive == false) {
         filteredTrainers = filteredTrainers.filter(t => t.currentlyActive === false);
-        logger.emit('log', `GET Currently Active students.`);
+        logger.emit('log', `GET Currently Non-Active trainers.`);
 
     }
     if (params.currentlyActive === true) {
         filteredTrainers = filteredTrainers.filter(t => t.currentlyActive === true)
-        logger.emit('log', `GET Currently Non-Active students.`);
+        logger.emit('log', `GET Currently Active trainers.`);
 
     }
     // Sort by Finished Courses Count using query param
     // Ascending
     if (params.sortBy === 'completedCoursesAsc') {
         filteredTrainers.sort((a, b) => a.coursesFinishedCount - b.coursesFinishedCount);
-        logger.emit('log', `GET students sorted by finished courses count in ascending order.`);
+        logger.emit('log', `GET trainers sorted by finished courses count in ASCENDING order.`);
 
     }
     // Descending
     if (params.sortBy === 'completedCoursesDesc') {
         filteredTrainers.sort((a, b) => b.coursesFinishedCount - a.coursesFinishedCount);
-        logger.emit('log', `GET students sorted by finished courses count in descending order.`);
+        logger.emit('log', `GET trainers sorted by finished courses count in DESCENDING order.`);
 
     }
     return filteredTrainers;
